df761667865ef42d198e81530f120ecda13ef65d
ellenfurman <ellenf205@gmail.com>
2021-05-12 17:00:18 +0300

    Third

--- mat4.py
+++ mat4.py
@@ -16,42 +16,59 @@ mylist=list()
 information=tuple()
 i=0
 all_dis=list()
+all_cities_names=list()
 
 for city in Lines:
     #Request and receive data from web pages
+    #Integrity check 
     mydictionary['destinations'] = city
-    url1 = service_url + urllib.parse.urlencode(mydictionary)
-    response=requests.get(url1).json()
-    address=city
-    url2="https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (address, api_key)
-    response2 = requests.get(url2).json()
-    
-    #saving the data in variables
-    km=response['rows'][0]['elements'][0]['distance']['text']
-    time=response['rows'][0]['elements'][0]['duration']['text']
-    latitude=response2['results'][0]['geometry']['location']['lat']
-    longitude=response2['results'][0]['geometry']['location']['lng'] 
-    
-    #make a list with all the cities
-    dictionary=dict()
-    mylist.append(dictionary)
-    information=('distance:'+ km, 'time:' +time, 'longitude:' ,longitude, 'latitude:' ,latitude)
-    dictionary[city]=information
-    print(dictionary)
-    
-    #make a list with all the distances
-    all_dis.append(km)  
-    i+=1
-
+    try:
+        url1 = service_url + urllib.parse.urlencode(mydictionary)
+        response=requests.get(url1)
+        address=city
+        url2="https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (address, api_key)
+        response2 = requests.get(url2)
+        if response.status_code != 200 or response2.status_code !=200 :
+            print("HTTP Error")
+        else:
+            response=response.json()
+            response2=response2.json()
+            
+            #saving the data in variables
+            km=response['rows'][0]['elements'][0]['distance']['text']
+            time=response['rows'][0]['elements'][0]['duration']['text']
+            latitude=response2['results'][0]['geometry']['location']['lat']
+            longitude=response2['results'][0]['geometry']['location']['lng'] 
+            
+            #make a list with all the cities
+            dictionary=dict()
+            mylist.append(dictionary)
+            information=('distance:'+ km, 'time:' +time, 'longitude:', longitude, 'latitude:' ,latitude)
+            dictionary[city]=information
+            print("The destination is: " + city)
+            print(" the distance is: " + km)
+            print( " Duration of arrival to the destination: " + time)
+            print( " The meridians are: "+ str(longitude) )
+            print(" The latitudes are: "+ str(latitude))
+            print()
+            
+            #make a list with all the distances
+            #make a list with all the cities names
+            all_dis.append(km)
+            all_cities_names.append(city)
+            i+=1
+    except:
+        print("Requests error")
+            
 #Choose the three cities with the largest distance from Tel Aviv and print them
 the_3_Cities=list()   
 for km_num in all_dis:
    while len(the_3_Cities)<3 : 
        big_num=max(all_dis) 
-       the_3_Cities.append(big_num)
        index=all_dis.index(max(all_dis))
+       the_3_Cities.append(all_cities_names[index])
        all_dis[index]= '0' 
-print(the_3_Cities)   
+print("The three cities with the largest distance from Tel Aviv: "+ str(the_3_Cities))   
     
 
 
